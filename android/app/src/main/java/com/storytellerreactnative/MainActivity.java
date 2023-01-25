d6f9c8623887e1d9cbdf59f212bafe5278ee249a
jxie93 <zhihao420@hotmail.com>
2023-01-25 12:36:15 +0000

    Update sdk to 8.5.0

--- android/app/src/main/java/com/storytellerreactnative/MainActivity.java
+++ android/app/src/main/java/com/storytellerreactnative/MainActivity.java
@@ -1,6 +1,11 @@
 package com.storytellerreactnative;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import com.facebook.react.ReactActivity;
+import com.storyteller.reactnative.StorytellerModule;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +16,20 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "StorytellerReactNative";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    View decorView = getWindow().getDecorView();
+    decorView.setSystemUiVisibility(
+            decorView.getSystemUiVisibility() | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    );
+  }
+
+  @Override
+  public void onActivityReenter(int resultCode, Intent data) {
+    super.onActivityReenter(resultCode, data);
+    StorytellerModule.Companion.activityReentered(this);
   }
 }
